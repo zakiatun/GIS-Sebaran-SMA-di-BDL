89c7a08d691b46fb0f9cd67bcc52c601f4df87dc
anitadmhrn <anitadwimaharani8@gmail.com>
2018-04-04 16:15:01 +0700

    edit

---
+++ inc/config.php
@@ -0,0 +1,16 @@
+
+<?php
+
+
+/*
+SISTEM INFORMASI GEOGRAFIS SEBARAN SEKOLAH MENENGAH ATAS DI BANDAR LAMPUNG
+*/
+	define('db_host','localhost');
+	define('db_user','root');
+	define('db_pass','');
+	define('db_name','gmaps');
+	
+	mysql_connect(db_host,db_user,db_pass);
+	mysql_select_db(db_name);
+	
+?>
